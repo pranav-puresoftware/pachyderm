725e6c46eb0289b8bebefadf6135b0c97d56377e
pranav-puresoftware <60650076+pranav-puresoftware@users.noreply.github.com>
2020-07-28 19:34:29 +0530

    Update travis_build_check_and_stash.sh

--- etc/testing/travis_build_check_and_stash.sh
+++ etc/testing/travis_build_check_and_stash.sh
@@ -73,7 +73,7 @@ fi
 
 docker login -u pachydermbuildbot -p "${DOCKER_PWD}"
 
-cd /home/travis/gopath/src/github.com/pranav-puresoftwar
+cd /home/travis/gopath/src/github.com/pranav-puresoftware
 mkdir -p /tmp/save_git_tarball
 tar cf /tmp/save_git_tarball/pachyderm.tar pachyderm
 cd /tmp/save_git_tarball
